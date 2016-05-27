85d002dda089f9a7b1506518fa95fac4d9a931f7
wellcheng <chengwei06@baidu.com>
2016-05-27 23:16:25 +0800

    new project

---
+++ SISpeciesNotes/SISpeciesNotes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SISpeciesNotes
+//
+//  Created by WellCheng on 16/5/27.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SISpeciesNotes/SISpeciesNotes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SISpeciesNotes
+//
+//  Created by WellCheng on 16/5/27.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
